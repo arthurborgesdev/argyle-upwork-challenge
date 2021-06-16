69428a19261c17b0cd0a1f4ca99248b4c25d3ba6
Arthur Borges <arthurborges.dev@gmail.com>
2021-06-16 11:06:53 -0300

    Solve and generate captcha token

--- spider/cookies.py
+++ spider/cookies.py
@@ -1,46 +1,47 @@
-{
-  {
-    "name": "visitor_signup_gql_token",
-    "value": "oauth2v2_7a89c810d2e1ab969420a3d6b212e39d",
-    "domain": ".upwork.com",
-    "path": "/",
-    "expires": -1,
-    "httpOnly": False,
-    "secure": True,
-    "sameSite": "None",
-  },
 
-  {
-    "name": "visitor_signup_gql_token",
-    "value": "oauth2v2_7a89c810d2e1ab969420a3d6b212e39d",
-    "domain": ".upwork.com",
-    "path": '/',
-    "expires": -1,
-    "httpOnly": False,
-    "secure": True,
-    "sameSite": 'None',
-  },
 
-  {
-    "name": 'visitor_signup_gql_token',
-    "value": 'oauth2v2_7a89c810d2e1ab969420a3d6b212e39d',
-    "domain": '.upwork.com',
-    "path": '/',
-    "expires": -1,
-    "httpOnly": False,
-    "secure": True,
-    "sameSite": 'None',
-  },
+cookies = {
+    {
+      "name": "visitor_signup_gql_token",
+      "value": "oauth2v2_7a89c810d2e1ab969420a3d6b212e39d",
+      "domain": ".upwork.com",
+      "path": "/",
+      "expires": -1,
+      "httpOnly": False,
+      "secure": True,
+      "sameSite": None,
+    },
+ 
+    {
+        "name": "visitor_signup_gql_token",
+        "value": "oauth2v2_7a89c810d2e1ab969420a3d6b212e39d",
+        "domain": ".upwork.com",
+        "path": '/',
+        "expires": -1,
+        "httpOnly": False,
+        "secure": True,
+        "sameSite": 'None',
+    },
+    
+    {
+        "name": 'visitor_signup_gql_token',
+        "value": 'oauth2v2_7a89c810d2e1ab969420a3d6b212e39d',
+        "domain": '.upwork.com',
+        "path": '/',
+        "expires": -1,
+        "httpOnly": False,
+        "secure": True,
+        "sameSite": 'None',
+    },
 
-  {
-    "name": 'visitor_signup_gql_token',
-    "value": 'oauth2v2_7a89c810d2e1ab969420a3d6b212e39d',
-    "domain": '.upwork.com',
-    "path": '/',
-    "expires": -1,
-    "httpOnly": False,
-    "secure": True,
-    "sameSite": 'None',
-  },
-
+    {
+        "name": 'visitor_signup_gql_token',
+        "value": 'oauth2v2_7a89c810d2e1ab969420a3d6b212e39d',
+        "domain": '.upwork.com',
+        "path": '/',
+        "expires": -1,
+        "httpOnly": False,
+        "secure": True,
+        "sameSite": 'None',
+    },
 }
