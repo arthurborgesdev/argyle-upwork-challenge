e080ddd2c5fd108596f13d87eee7df04998348bd
Arthur Borges <arthurborges.dev@gmail.com>
2021-06-18 20:43:01 -0300

    Refactor the main method, extracting functions in outside methods

--- spider/spider.py
+++ spider/spider.py
@@ -10,6 +10,18 @@ user_agent = ("Mozilla/5.0 (X11; Linux x86_64)"
               "Chrome/90.0.4430.93 Safari/537.36"
               )
 
+def login_routine(page):
+    login = LoginHandling()
+    login.username_login(page)
+    login.password_login(page)
+    login.secret_login(page)
+
+def scan_routine(user, page):
+    main = MainPage(user)
+    main.scan_main_page(page)
+    profile = ProfilePage(user)
+    profile.scan_profile_page(page)
+
 with sync_playwright() as p:
     # Apply slow_mo delay, so we don't need to solve reCaptcha
     browser = p.chromium.launch(headless=True, slow_mo=100)
@@ -18,16 +30,10 @@ with sync_playwright() as p:
         user_agent=user_agent
     )
     page = context.new_page()
-    login = LoginHandling()
-    login.username_login(page)
-    login.password_login(page)
-    login.secret_login(page)
 
+    login_routine(page)
     user = User()
-    main = MainPage(user)
-    main.scan_main_page(page)
-    profile = ProfilePage(user)
-    profile.scan_profile_page(page)
+    scan_routine(user, page)
 
     with open('scan_data.json', 'w') as outfile:
         json.dump(user.dict(), outfile)
